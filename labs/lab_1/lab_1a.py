13bce4f7eeb9d3dbd01941abc710f81f04357fda
cstco <44740705+cstco@users.noreply.github.com>
2026-04-01 10:53:34 -0700

    Update lab_1a.py

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -1,4 +1,4 @@
-"This is to simulate a change made on a robot: robot_speed = 5 # m/s""
+"This is to simulate a change made on a robot: robot_speed = 8 # m/s""
 lab_1a.py
 
 The first lab in the BWSI CSS course. To complete this lab, fill out the variable on line 10
@@ -8,7 +8,7 @@ with your name. Then, save the code, add it to the staging area, and commit it t
 def main():
     print("Hello World!")
 
-    name = "Nathan" # TODO: Insert your name between the double quotes
+    name = "" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
 
